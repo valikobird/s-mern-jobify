9286164e39e477da7640701d2fb0237626056c6a
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-12 16:22:18 +0200

    Implement flying sidebar

--- client/src/components/SmallSidebar/SmallSidebar.tsx
+++ client/src/components/SmallSidebar/SmallSidebar.tsx
@@ -1,9 +1,42 @@
 import Wrapper from './Wrapper';
+import { FaTimes } from 'react-icons/fa';
+import Logo from '../Logo';
+import links from '../../utils/links';
+import { NavLink } from 'react-router-dom';
+import { useDashboardContext } from '../../pages/DashboardLayout';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div className={`sidebar-container ${showSidebar ? 'show-sidebar' : ''}`}>
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={text}
+                  className="nav-link"
+                  onClick={toggleSidebar}
+                  end
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
