26b8a0e26c392cd20c1b26363ee4d5ede57d5044
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-13 16:11:47 +0200

    Implement DB connection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
@@ -33,6 +34,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
