e648ec8b86396d2c84c0fac33f550a33985fafc1
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-13 14:43:48 +0200

    Create endpoint to create job

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ import express from 'express';
 const app = express();
 
 import morgan from 'morgan';
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 
 let jobs = [
-  {id: nanoid(), company: 'apple', position: 'front-end'},
-  {id: nanoid(), company: 'google', position: 'back-end'},
-]
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+];
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -23,12 +23,27 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  res.json({message: 'data received', data: req.body});
-})
+  res.json({ message: 'data received', data: req.body });
+});
 
+// GET ALL JOBS
 app.get('/api/v1/jobs', (req, res) => {
-  res.status(200).json({jobs});
-})
+  res.status(200).json({ jobs });
+});
+
+// CREATE JOB
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' });
+  }
+
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+
+  res.status(200).json({ job });
+});
 
 const port = process.env.PORT || 5100;
 
