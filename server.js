179f61aa127a63eef5c7dc2bb9b40c16eb6dddbb
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-13 14:51:23 +0200

    Create endpoint to get a job

--- server.js
+++ server.js
@@ -42,11 +42,21 @@ app.post('/api/v1/jobs', (req, res) => {
   const job = { id, company, position };
   jobs.push(job);
 
+  res.status(201).json({ job });
+});
+
+// GET SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
   res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5100;
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
