5de62ac5eea5c118a78c07b1ba52a6fee6093c38
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-14 15:16:59 +0200

    Implement job creation

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,4 @@
+import Job from '../models/JobModel.js';
 import { nanoid } from 'nanoid';
 
 let jobs = [
@@ -9,16 +10,9 @@ export const getAllJobs = (req, res) => {
   res.status(200).json({ jobs });
 };
 
-export const createJob = (req, res) => {
+export const createJob = async (req, res) => {
   const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-
+  const job = await Job.create({ company, position });
   res.status(201).json({ job });
 };
 
