ca3f1b853ed951331df23bba5077ff66968680e5
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-16 12:38:12 +0200

    Implement user password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,17 @@
 import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
 import { USER_ROLES } from '../utils/constants.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? USER_ROLES.ADMIN : USER_ROLES.USER;
 
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: 'User registered' });
 };
 
 export const login = (req, res) => {
