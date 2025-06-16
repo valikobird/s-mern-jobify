3de86c49ec4623b53d3f54f4e7a163ca15b371f7
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-16 14:49:01 +0200

    Link user to the job and verify user

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
@@ -11,18 +12,20 @@ import authRouter from './routes/authRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Test');
 });
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.all(/.*/, (req, res) => {
