e4eabf6dae423153bbbd0e105dbd2ea2189994c5
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-16 14:31:02 +0200

    Implement user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
 import { USER_ROLES } from '../utils/constants.js';
-import { hashPassword } from '../utils/passwordUtils.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,6 +16,30 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: 'User registered' });
 };
 
-export const login = (req, res) => {
-  res.send('login');
+export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    throw new UnauthenticatedError('invalid credentials');
+  }
+
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password,
+  );
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('invalid credentials');
+  }
+
+  const token = createJWT({
+    userId: user._id,
+    role: user.role,
+  });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.status(StatusCodes.OK).json({ msg: 'User login successfully' });
 };
