cc5ca7f374f8b0b14c9d219750c8b3678b4e6f0b
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-16 15:03:36 +0200

    Restrict jobs fetching by user; Add user validation on fetching jobs

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,6 +1,10 @@
 import { body, param, validationResult } from 'express-validator';
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
-import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../errors/customErrors.js';
+import { JOB_STATUS, JOB_TYPE, USER_ROLES } from '../utils/constants.js';
 import mongoose from 'mongoose';
 import Job from '../models/JobModel.js';
 import User from '../models/UserModel.js';
@@ -14,6 +18,10 @@ const withValidationErrors = (validateValues) => {
         const errorMessages = errors.array().map((error) => error.msg);
         if (errorMessages[0].startsWith('no job')) {
           throw new NotFoundError(errorMessages);
+        }
+
+        if (errorMessages[0].startsWith('not authorized')) {
+          throw new UnauthorizedError('not authorized to access this job');
         }
 
         throw new BadRequestError(errorMessages);
@@ -37,7 +45,7 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParam = withValidationErrors([
-  param('id').custom(async (value) => {
+  param('id').custom(async (value, { req }) => {
     const isValidId = mongoose.Types.ObjectId.isValid(value);
     if (!isValidId) {
       throw new BadRequestError('invalid MongoDB id');
@@ -46,6 +54,13 @@ export const validateIdParam = withValidationErrors([
     const job = await Job.findById(value);
     if (!job) {
       throw new NotFoundError(`no job with id ${value}`);
+    }
+
+    const isAdmin = req.user.role === USER_ROLES.ADMIN;
+    const isOwner = req.user.userId === job.createdBy.toString();
+
+    if (!isAdmin && !isOwner) {
+      throw new UnauthorizedError('not authorized to access this job');
     }
   }),
 ]);
