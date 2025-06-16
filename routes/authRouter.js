d95f83502f988b34719a5d0b78c0a05696f1c72f
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-16 12:47:13 +0200

    Implement user login data validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,9 +2,12 @@ import { Router } from 'express';
 const router = Router();
 
 import { login, register } from '../controllers/authController.js';
-import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+import {
+  validateLoginInput,
+  validateRegisterInput,
+} from '../middleware/validationMiddleware.js';
 
 router.post('/register', validateRegisterInput, register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
 
 export default router;
