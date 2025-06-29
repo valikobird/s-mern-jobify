c94019831a9063a688f82003678e370cb0abba82
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-29 16:25:37 +0200

    Implement test user login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,8 +1,31 @@
 import Wrapper from './Wrapper';
 import { FormRow, Logo, SubmitBtn } from '../../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate } from 'react-router-dom';
+import customFetch from '../../utils/customFetch.ts';
+import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    };
+
+    try {
+      await customFetch.post('/auth/login', data);
+      toast.success('Explore the app');
+      navigate('/dashboard');
+    } catch (err) {
+      const axiosError = err as AxiosError;
+      if (axiosError.isAxiosError) {
+        toast.error(axiosError?.response?.data?.msg);
+      }
+    }
+  };
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -11,7 +34,7 @@ const Login = () => {
         <FormRow type="email" name="email" defaultValue="test@nusho.tut" />
         <FormRow type="password" name="password" defaultValue="secret123" />
         <SubmitBtn />
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
