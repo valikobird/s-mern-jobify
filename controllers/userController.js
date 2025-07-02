d73e8481c580a709f3d9d00ce6139da508d39e0f
Valentyn Zozulia <valikobird@protonmail.com>
2025-07-02 12:55:26 +0200

    Switch image upload from file to memory buffer

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 import { StatusCodes } from 'http-status-codes';
 import { v2 as cloudinary } from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user.userId);
@@ -21,8 +21,8 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
