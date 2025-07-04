ae523457d2ba843c0e047caa96f6e37dda7f209b
Valentyn Zozulia <valikobird@protonmail.com>
2025-07-04 13:29:42 +0200

    Add sanitizer, rate limiter and helmet

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,13 +2,18 @@ import { Router } from 'express';
 const router = Router();
 
 import { login, logout, register } from '../controllers/authController.js';
-import {
-  validateLoginInput,
-  validateRegisterInput,
-} from '../middleware/validationMiddleware.js';
+import { validateLoginInput, validateRegisterInput } from '../middleware/validationMiddleware.js';
+import rateLimiter from 'express-rate-limit';
 
-router.post('/register', validateRegisterInput, register);
-router.post('/login', validateLoginInput, login);
+const TIMEOUT_MIN = 15;
+const apiLimiter = rateLimiter({
+  windowMs: TIMEOUT_MIN * 60 * 1000,
+  max: 10,
+  message: { msg: `IP rate limit exceeded, retry in ${TIMEOUT_MIN} minutes` },
+});
+
+router.post('/register', apiLimiter, validateRegisterInput, register);
+router.post('/login', apiLimiter, validateLoginInput, login);
 router.get('/logout', logout);
 
 export default router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
@@ -35,18 +37,16 @@ app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.use(cookieParser());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Test');
-});
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+// });
 
 app.all(/.*/, (req, res) => {
   res.status(404).send('Not found');
